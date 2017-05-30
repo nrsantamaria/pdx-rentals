8ed403400bd0256ddae793c8bef46c388ceb43e0
jin camou and Nicole Santamaria <nicole@>
2017-05-30 11:33:12 -0700

    add code to delete record

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,4 +5,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
   },
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
